b445edc3318db9ed7bb9867e8610711eb8eca420
Poserko <kokolin2204@gmail.com>
2023-02-08 09:09:09 +0200

    First Commit

---
+++ StatArr/StatArr/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace StatArr
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                arr[i] = int.Parse(Console.ReadLine());
+
+
+            }
+            Console.WriteLine(OptionManeger.CountLess100(arr));
+            Console.WriteLine(OptionManeger.MaxNum(arr));
+            arr = OptionManeger.ReverseArr(arr);
+            OptionManeger.PrintArr(arr);        
+        }
+    }
+}
