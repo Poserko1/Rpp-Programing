c5fb3e49611024cf58a729dd6ac131d1156c6015
poserko <kokolin2204@gmail.com>
2023-02-15 09:40:30 +0200

    Zad Spisuci

---
+++ ListZad2/ZadSp2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace zad_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> nums = Console.ReadLine().Split('|').ToList();
+            nums.Reverse();
+            List<string> final = new List<string>();
+
+            for (int i = 0; i < nums.Count; i++)
+            {
+                List<string> arr = nums[i].Split(' ', StringSplitOptions.RemoveEmptyEntries).ToList();
+                foreach (string num in arr)
+                {
+                    final.Add(num);
+                }
+            }
+            Console.WriteLine(string.Join(" ", final));
+        }
+    }
+}
